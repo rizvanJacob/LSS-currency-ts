1eb9aa63723e5c2a29b28d11eb6c3c9574b3cc49
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-12 10:57:13 +0800

    access NRIC and DOB using sg_id

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,13 +11,30 @@ const clientConfig = {
   clientId: process.env.CLIENT_ID as string,
   clientSecret: process.env.CLIENT_SECRET as string,
   privateKey: process.env.PRIVATE_KEY as string,
-  redirectUri: "http://localhost:3000",
+  redirectUri: "http://localhost:3000/",
 };
 
 const client = new SgidClient(clientConfig);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(JSON.stringify(client));
+const { url } = client.authorizationUrl(
+  "state",
+  ["openid", "myinfo.nric_number", "myinfo.date_of_birth"],
+  null
+);
+
+app.get("/auth", (req: Request, res: Response) => {
+  res.redirect(url);
+});
+
+app.get("/", async (req: Request, res: Response) => {
+  const { code } = req.query;
+  try {
+    const { accessToken } = await client.callback(code as string);
+    const { data } = await client.userinfo(accessToken as string);
+    res.send(`data: ${JSON.stringify(data)}`);
+  } catch (error) {
+    res.redirect("/auth");
+  }
 });
 
 app.get("/api", (req: Request, res: Response) => {
