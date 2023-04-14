490c50a09f6d3544a30b8199b63b9d28cd7acea0
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-14 17:29:46 +0800

    set up server middleware and routers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,37 +1,25 @@
 import express from "express";
-import { Request, Response } from "express";
+import logger from "morgan";
+import path from "path";
 import * as dotenv from "dotenv";
 dotenv.config();
-
-import client from "./config/sgid";
-import { prisma, connectDb } from "./config/database";
+import { connectDb } from "./config/database";
 connectDb();
 
-const PORT = 3000;
+// import usersRouter from "./routes/usersRouter"
+// import traineesRouter from "./routes/traineesRouter"
+// import trainingsRouter from "./routes/trainingsRouter"
+
 const app = express();
 
-const { url } = client.authorizationUrl(
-  "state",
-  ["openid", "myinfo.nric_number", "myinfo.date_of_birth"],
-  null
-);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("/auth", (req: Request, res: Response) => {
-  res.redirect(url);
-});
+// app.use("/api/users", usersRouter)
+// app.use("/api/trainees", traineesRouter)
+// app.use("/api/trainings", trainingsRouter)
 
-app.get("/", async (req: Request, res: Response) => {
-  const { code } = req.query;
-  try {
-    const { accessToken } = await client.callback(code as string);
-    const { data } = await client.userinfo(accessToken as string);
-    res.send(`data: ${JSON.stringify(data)}`);
-  } catch (error) {
-    res.redirect("/auth");
-  }
-});
-app.get("/api", (req: Request, res: Response) => {
-  res.send("hello !!!!");
-});
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`start listening on port : ${PORT}`));
