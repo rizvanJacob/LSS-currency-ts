93cf7d8b4e0806114f386310bde220f374a59a3f
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-17 16:10:37 +0800

    log in and sign jwt token

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,4 +1,9 @@
 import SgidClient from "@opengovsg/sgid-client";
+import { prisma } from "../config/database";
+import { Request, Response } from "express";
+import * as jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 const client = new SgidClient({
   clientId: process.env.CLIENT_ID as string,
@@ -7,26 +12,60 @@ const client = new SgidClient({
   redirectUri: "http://localhost:3000/api/login",
 });
 
-const generateUrl = (req: any, res: any) => {
-  const url = client.authorizationUrl(
-    "state",
-    process.env.SCOPE as string,
-    null
-  );
+const generateUrl = (req: Request, res: Response) => {
+  const url = client.authorizationUrl("state", "openid", null);
 
   res.status(200).json(url);
 };
 
-const login = async (req: any, res: any) => {
+const login = async (req: Request, res: Response) => {
   const { code } = req.query;
-  const { accessToken } = await client.callback(code, null);
-  const { data } = await client.userinfo(accessToken);
+  try {
+    const { sub: openId } = await client.callback(code as string, null);
+    try {
+      const userData = await prisma.user.findUniqueOrThrow({
+        where: { openId } as any,
+        select: {
+          id: true,
+          displayName: true,
+          authCategory: true,
+          accountType: true,
+        },
+      });
 
-  res.send(JSON.stringify(data));
+      const token = await jwt.sign(userData, JWT_SECRET, { expiresIn: "1h" });
+      res.status(200).json(token);
+    } catch (error) {
+      //database not available
+      console.log(error);
+      res.status(500);
+    }
+  } catch (error) {
+    //SG ID client unavailable
+    console.log(error);
+    res.redirect("/api");
+  }
+};
+
+const findUser = async (req: Request, res: Response) => {
+  const { openId } = req.params;
+
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: { openId } as any,
+    select: {
+      id: true,
+      displayName: true,
+      authCategory: true,
+      accountType: true,
+    },
+  });
+
+  const token = await jwt.sign(userData, JWT_SECRET, { expiresIn: "1h" });
+  res.json(token);
 };
 
 const isAuth = () => {};
 
 const isUser = () => {};
 
-export { generateUrl, isAuth, isUser, login };
+export { generateUrl, isAuth, isUser, login, findUser };
--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -5,5 +5,6 @@ import * as authController from "../controllers/authController";
 
 router.get("/", authController.generateUrl);
 router.get("/login", authController.login);
+router.get("/find/:openId", authController.findUser);
 
 export default router;
