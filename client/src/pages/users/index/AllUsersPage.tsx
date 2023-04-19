6c9043e1e6d0ba84f8449e9fb3970f4d7416d71a
GREGFOO\User <gregoryfoo@hotmail.com>
2023-04-19 21:57:45 +0800

    feat: Separate userslist into pending approval & approved

--- client/src/pages/users/index/AllUsersPage.tsx
+++ client/src/pages/users/index/AllUsersPage.tsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import UsersList from "./UsersList";
+import ApprovedUsersList from "./ApprovedUsersList";
 import getRequest from "../../../utilities/getRequest";
 import { SimpleLookup } from "../../../@types/lookup";
 import { User } from "../../../@types/user";
-
+import FilteredUsersList from "./UnapprovedUsersList"
 
 export default function AllUsersPage(): JSX.Element {
   const [users, setUsers] = useState<User[]>([]);
@@ -14,13 +14,24 @@ export default function AllUsersPage(): JSX.Element {
     getRequest(`/api/lookup/accountTypes`, setAccountTypes);
   }, []);
 
+  const notApprovedUsers = users.filter(user => !user.approved);
+  const approvedUsers = users.filter(user => user.approved);
+
   return (
     <>
-      <h1> Display all users for Admin to see</h1>
       {users.length > 0 ? (
         <>
-          <UsersList
-            users={users as User[]}
+          <h1>Pending approval:</h1>
+          <FilteredUsersList
+            users={notApprovedUsers as User[]}
+            setUsers={
+              setUsers as React.Dispatch<React.SetStateAction<User[]>>
+            }
+            accountTypes = {accountTypes as SimpleLookup[]}
+          />
+          <h1> Approved Users:</h1>
+          <ApprovedUsersList
+            users={approvedUsers as User[]}
             setUsers={
               setUsers as React.Dispatch<React.SetStateAction<User[]>>
             }
@@ -30,7 +41,6 @@ export default function AllUsersPage(): JSX.Element {
       ) : (
         <>
           <p>
-            {" "}
             <progress />
           </p>
         </>
