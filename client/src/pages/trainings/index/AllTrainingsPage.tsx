35877606f9fa6fd83f23b907d4c0df68e0208939
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-26 17:32:42 +0800

    fix issues post merge

--- client/src/pages/trainings/index/AllTrainingsPage.tsx
+++ client/src/pages/trainings/index/AllTrainingsPage.tsx
@@ -1,42 +1,40 @@
 import { Account } from "../../../../../server/src/constants";
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 import getRequest from "../../../utilities/getRequest";
 import { Training } from "../../../@types/training";
-import TrainingList from "./TrainingList"
+import TrainingList from "./TrainingList";
 import CreateTrainingButton from "../create/CreateTrainingButton";
 import { CurrentUser } from "../../../@types/currentUser";
 import { CurrentUserContext } from "../../../App";
 export default function AllTrainingsPage(): JSX.Element {
-    const [trainings, setTrainings] = useState<Training[]>([]);
-    const currentUser = useContext<CurrentUser | null>(CurrentUserContext);
+  const [trainings, setTrainings] = useState<Training[]>([]);
+  const currentUser = useContext<CurrentUser | null>(CurrentUserContext);
 
-    useEffect(() => {
-        getRequest(`/api/trainings`, setTrainings);
-    }, [])
-    
-    return (
-        <>
-            {trainings.length > 0 ? (
-            <>
-                <h1>Training Index</h1>
-                {currentUser?.accountType === Account.Trainer ? <CreateTrainingButton /> : <></>}
-                <TrainingList trainings = { trainings as Training[] } setTrainings = { setTrainings as React.Dispatch<React.SetStateAction<Training[]>>} />
-            </>
-            ) : (
-            <>
-                <h1>Fetching Trainings</h1>
-                <p><progress /></p>
-            </>
-            )}
-        </>
+  useEffect(() => {
+    getRequest(`/api/trainings`, setTrainings);
+  }, []);
+
+  return trainings.length > 0 ? (
+    <>
+      <h1>Training Index</h1>
+      {currentUser?.accountType === Account.Trainer ? (
+        <CreateTrainingButton />
       ) : (
-        <>
-          <h1>Fetching Trainings</h1>
-          <p>
-            <progress className="progress w-56" />
-          </p>
-        </>
+        <></>
       )}
+      <TrainingList
+        trainings={trainings as Training[]}
+        setTrainings={
+          setTrainings as React.Dispatch<React.SetStateAction<Training[]>>
+        }
+      />
+    </>
+  ) : (
+    <>
+      <h1>Fetching Trainings</h1>
+      <p>
+        <progress className="progress w-56" />
+      </p>
     </>
   );
 }
--- server/src/routes/trainingsRouter.ts
+++ server/src/routes/trainingsRouter.ts
@@ -1,15 +1,34 @@
-import {Account} from "../constants"
+import { Account } from "../constants";
 import express from "express";
 const router = express.Router();
 import trainingsController from "../controllers/trainingsController";
 import { isAuth } from "../controllers/authController";
 
-router.get("/", isAuth([Account.Admin, Account.TraineeAdmin, Account.Trainer]), trainingsController.getAllTrainings);
-router.get("/:id", isAuth([Account.Admin, Account.TraineeAdmin, Account.Trainer]), trainingsController.showTraining);
-router.put("/:id", isAuth([Account.Admin, Account.Trainer]), trainingsController.updateTraining);
-router.delete("/:id", isAuth([Account.Admin, Account.Trainer]), trainingsController.deleteTraining);
-router.post("/", isAuth([Account.Admin, Account.Trainer]), trainingsController.createTraining);
-router.get("/testCurrencyUpdate/:id", trainingsController.testCurrencyUpdate);
+router.get(
+  "/",
+  isAuth([Account.Admin, Account.TraineeAdmin, Account.Trainer]),
+  trainingsController.getAllTrainings
+);
+router.get(
+  "/:id",
+  isAuth([Account.Admin, Account.TraineeAdmin, Account.Trainer]),
+  trainingsController.showTraining
+);
+router.put(
+  "/:id",
+  isAuth([Account.Admin, Account.Trainer]),
+  trainingsController.updateTraining
+);
+router.delete(
+  "/:id",
+  isAuth([Account.Admin, Account.Trainer]),
+  trainingsController.deleteTraining
+);
+router.post(
+  "/",
+  isAuth([Account.Admin, Account.Trainer]),
+  trainingsController.createTraining
+);
 router.put("/complete/:id", trainingsController.completeTraining);
 
 export default router;
