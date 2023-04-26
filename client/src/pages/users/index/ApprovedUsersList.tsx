c1b20c163286c401491ec38bd318f4b788349b5d
GREGFOO\User <gregoryfoo@hotmail.com>
2023-04-26 20:28:53 +0800

    style: Change hamburger icon and cross icon to navbar

--- client/src/pages/users/index/ApprovedUsersList.tsx
+++ client/src/pages/users/index/ApprovedUsersList.tsx
@@ -36,7 +36,7 @@ export default function ApprovedUsersList({
             }
             return (
               <tr key={user.id} className="hover:bg-gray-100">
-                <td className="px-2 py-4 whitespace-nowrap text-sm font-medium text-blue-200">{user.displayName}</td>
+                <td className="px-2 py-4 whitespace-nowrap text-sm font-medium text-blue-600">{user.displayName}</td>
                 <td className={`px-2 py-4 whitespace-nowrap text-sm hidden md:table-cell ${accountTypeClass}`}>{accountType}</td>
                 <td className="px-1 py-2 whitespace-nowrap"><EditUserButton user={user} /></td>
                 <td className="px-1 py-2 whitespace-nowrap"><DeleteUserButton setUsers={setUsers} user={user} /></td>
