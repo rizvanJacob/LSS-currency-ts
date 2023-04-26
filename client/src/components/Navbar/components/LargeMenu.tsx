bf196d35ed5026a8b530b1be4de5a770511d49fe
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-26 18:37:37 +0800

    complete navbar styling

--- client/src/components/Navbar/components/LargeMenu.tsx
+++ client/src/components/Navbar/components/LargeMenu.tsx
@@ -9,11 +9,12 @@ type Prop = {
 const LargeMenu = ({ menuItems, className }: Prop) => {
   return (
     <div className={className}>
-      {menuItems.map((i) => {
+      {menuItems.map((i, index) => {
         return (
           <Link
             className="btn btn-ghost normal-case text-md text-neutral-200 sm:text-lg"
             to={i.path}
+            key={index}
           >
             {i.name}
           </Link>
