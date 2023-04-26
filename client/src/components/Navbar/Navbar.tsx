9b0a9314b10246ba59745a99cd985ab7e275fb8b
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-26 18:26:47 +0800

    style nav bar

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Link, useNavigate } from "react-router-dom";
+import SmallMenu from "./components/SmallMenu";
+import LargeMenu from "./components/LargeMenu";
+
+export type MenuItem = {
+  name: string;
+  path: string;
+};
+
+const Navbar = (): JSX.Element => {
+  const navigate = useNavigate();
+  const menuItems: MenuItem[] = [
+    { name: "Users", path: "/users" },
+    { name: "Trainees", path: "/trainees" },
+    { name: "Trainers", path: "/trainers" },
+    { name: "Log Out", path: "/" },
+  ];
+
+  return (
+    <nav className="navbar bg-cyan-800 max-h-10">
+      <div className="flex-1">
+        <button
+          className="btn btn-sm btn-ghost normal-case text-md text-neutral-200 sm:text-lg"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          Back
+        </button>
+      </div>
+      <SmallMenu className="sm:hidden" menuItems={menuItems} />
+      <LargeMenu className="hidden sm:block" menuItems={menuItems} />
+    </nav>
+  );
+};
+
+export default Navbar;
