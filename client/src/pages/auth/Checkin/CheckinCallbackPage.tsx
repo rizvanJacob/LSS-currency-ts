bcef844ce13a94b99fce1af58c895cbfedb97b07
rizvan93 <84754905+rizvan93@users.noreply.github.com>
2023-04-27 14:47:56 +0800

    explicitly only allow trainees to check in

--- client/src/pages/auth/Checkin/CheckinCallbackPage.tsx
+++ client/src/pages/auth/Checkin/CheckinCallbackPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import { DecodedToken } from "../../../@types/currentUser";
 import CheckinToTraining from "./CheckinToTraining";
@@ -7,7 +7,10 @@ import { Training } from "../../../@types/training";
 
 const CheckinCallbackPage = () => {
   const jwtoken = useRef("");
-  const user = useRef(0);
+  const user = useRef({
+    id: 0,
+    accountType: 0,
+  });
   const [trainings, setTrainings] = useState<Training[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const location = useLocation();
@@ -20,9 +23,9 @@ const CheckinCallbackPage = () => {
       jwtoken.current = data.token;
 
       const decodedToken = jwtDecode(data.token) as DecodedToken;
-      user.current = decodedToken.id;
+      user.current = decodedToken;
       const trainingsResponse = await fetch(
-        `/api/trainings/?checkin=true&user=${user.current}`,
+        `/api/trainings/?checkin=true&user=${user.current.id}`,
         { headers: { authorization: `bearer ${jwtoken.current}` } }
       );
       const trainingsData = await trainingsResponse.json();
@@ -32,7 +35,7 @@ const CheckinCallbackPage = () => {
     loginAndGetTrainings();
   }, []);
 
-  return (
+  return user.current.accountType === 3 ? (
     <div className="card w-96 shadow-xl mx-auto">
       <div className="card-body">
         <h1 className="card-title text-3xl text-center font-bold mb-8">
@@ -49,7 +52,7 @@ const CheckinCallbackPage = () => {
               <CheckinToTraining
                 training={t}
                 token={jwtoken.current}
-                user={user.current}
+                user={user.current.id}
                 setIsLoading={setIsLoading}
                 key={t.id}
               />
@@ -59,6 +62,13 @@ const CheckinCallbackPage = () => {
           <p>No trainings to check in to</p>
         )}
       </div>
+    </div>
+  ) : (
+    <div className="alert alert-error shadow-lg">
+      <h1 className="text-semibold">You must be a trainee to check in</h1>
+      <Link to="/">
+        <button className="btn btn-ghost">Back</button>
+      </Link>
     </div>
   );
 };
