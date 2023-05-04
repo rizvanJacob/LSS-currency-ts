0aeeb00877af7232c5e151ea501d2f68c7e18c76
Rizvan <rizzy@live.com.sg>
2023-05-04 11:06:50 +0800

    feat:add confirm delete modal for delete training button

--- client/src/pages/trainings/delete/DeleteTrainingButton.tsx
+++ client/src/pages/trainings/delete/DeleteTrainingButton.tsx
@@ -3,35 +3,57 @@ import { Training } from "../../../@types/training";
 import { useNavigate } from "react-router-dom";
 import { CurrentUser } from "../../../@types/currentUser";
 import { CurrentUserContext } from "../../../App";
-import { useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import RedCross from "../../../assets/icons/redCross.svg";
+import DialogModal from "../../../components/DialogModal";
+import dayjs from "dayjs";
 export default function DeleteTrainingButton({
   setTraining,
   training,
-  setTrainings
+  setTrainings,
 }: {
   setTraining: React.Dispatch<React.SetStateAction<Training>>;
   training: Training;
   setTrainings: React.Dispatch<React.SetStateAction<Training[]>>;
 }): JSX.Element {
-    const currentUser = useContext<CurrentUser | null>(CurrentUserContext)
-    const navigate = useNavigate();
+  const currentUser = useContext<CurrentUser | null>(CurrentUserContext);
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
-    const handleDeleteClick = async (id: number) => {
-        try {
-            await deleteRequest(`/api/trainings/${id}`, training.id, setTrainings);
-            navigate("/trainings", { replace: true });
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  const handleDeleteClick = async (id: number) => {
+    try {
+      await deleteRequest(`/api/trainings/${id}`, training.id, setTrainings);
+      navigate("/trainings", { replace: true });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    return (
+  return (
+    <>
       <button
-        onClick={() => handleDeleteClick(training.id)}
+        onClick={() => setShowModal(true)}
         className="flex-0 btn btn-square btn-sm btn-secondary text-white"
       >
         <img src={RedCross} alt="redCross" />
       </button>
-    );
+      {showModal && (
+        <DialogModal
+          title="Delete this training?"
+          message={`Are you sure you want to delete ${
+            training.requirements.name
+          } on ${dayjs(training.start).format(
+            "D MMM"
+          )}? This action cannot be undone.`}
+          isOpened={showModal}
+          proceedButtonText="Delete"
+          onProceed={() => handleDeleteClick(training.id)}
+          closeButtonText="Cancel"
+          onClose={() => {
+            setShowModal(false);
+          }}
+        />
+      )}
+    </>
+  );
 }
--- client/src/pages/trainings/index/TrainingList.tsx
+++ client/src/pages/trainings/index/TrainingList.tsx
@@ -15,19 +15,25 @@ export default function TrainingList({
 }: TrainingListProps): JSX.Element {
   return (
     <div className="overflow-x-auto">
-      <table className="table w-full">
+      <table className="table w-screen">
         <TrainingTableRow />
         <tbody className="bg-white divide-y divide-gray-200">
           {trainings.map((training: Training) => {
             return (
               <tr key={training.id} className="hover:bg-gray-100">
-                <td className="px-2 py-4 whitespace-nowrap text-center text-sm font-medium text-slate-950">{training.requirements.name}</td>
+                <td className="px-2 py-4 whitespace-nowrap text-center text-sm font-medium text-slate-950">
+                  {training.requirements.name}
+                </td>
                 <td className="px-2 py-4 whitespace-nowrap text-center text-sm font-medium text-slate-950">
                   {training.capacity - Object.keys(training.trainees).length}/
                   {training.capacity}
                 </td>
-                <td className="px-2 py-4 whitespace-nowrap text-center text-black text-sm hidden md:table-cell">{dayjs(training.start).format("YYYY-MM-DD, HH:mm a")}</td>
-                <td className="px-2 py-4 whitespace-nowrap text-center text-black text-sm hidden md:table-cell">{dayjs(training.end).format("YYYY-MM-DD, HH:mm a")}</td>
+                <td className="px-2 py-4 whitespace-nowrap text-center text-black text-sm hidden md:table-cell">
+                  {dayjs(training.start).format("YYYY-MM-DD, HH:mm a")}
+                </td>
+                <td className="px-2 py-4 whitespace-nowrap text-center text-black text-sm hidden md:table-cell">
+                  {dayjs(training.end).format("YYYY-MM-DD, HH:mm a")}
+                </td>
                 <td className="px-2 py-4 whitespace-nowrap text-center text-sm font-medium text-slate-950">
                   <ShowTrainingButton training={training} />
                 </td>
