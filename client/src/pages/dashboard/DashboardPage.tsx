a6230cbf83ad273ba57e3c8507ccebe541fd5c0b
unknown <gregoryfoo@hotmail.com>
2023-05-11 19:57:06 +0800

    feat: Data viz for training start/end date overview for training deconflict

--- client/src/pages/dashboard/DashboardPage.tsx
+++ client/src/pages/dashboard/DashboardPage.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect, useContext  } from "react";
 import { Trainee } from "../../@types/trainee";
+import { Training } from "../../@types/training";
 import getRequest from "../../utilities/getRequest";
 import BarChart from "../../components/Chart/BarChart"
+import ScatterPlot from "../../components/Chart/ScatterPlot";
 import ProgressBar from "../../components/ProgressBar";
 import { TitleContext } from "../../App";
 
 export default function DashboardPage(): JSX.Element {
     const [trainees, setTrainees] = useState<Trainee[]>([]);
+    const [trainings, setTrainings] = useState<Training[]>([]);
     const setTitle = useContext<React.Dispatch<
     React.SetStateAction<string>
   > | null>(TitleContext);
@@ -14,14 +17,13 @@ export default function DashboardPage(): JSX.Element {
     useEffect(() => {
         if (setTitle) setTitle("Data Viz");
         getRequest(`/api/trainees`, setTrainees);
+        getRequest(`/api/trainings`, setTrainings);
     }, [])
 
-    console.log(trainees);
-    return trainees.length ? (
+    return  (
         <>
-            <BarChart data = { trainees } />
+            {trainees.length ? <BarChart data = { trainees } /> : <ProgressBar />}
+            {trainings.length ? <ScatterPlot data = { trainings } /> : <ProgressBar />}
         </>
-    ) : (
-        <ProgressBar />
     )
 }
