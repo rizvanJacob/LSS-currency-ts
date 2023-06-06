3dbd2e6603ccce6b33bfb9ea629cfaf53a804a19
Theawmaster <105071602+Theawmaster@users.noreply.github.com>
2023-06-06 08:57:23 +0000

    problem_1_&_2

--- server/src/controllers/VAController.ts
+++ server/src/controllers/VAController.ts
@@ -1,22 +1,42 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/database";
 
-//Problem 1: write a function that will read all the entries in the "UserModel" table, including the name of the account type of the user. Respond to the HTTP request with a 200 status code and the data if successful. If no entries exist, respond with a status code of 400. If the function fails, respond with a status code of 500.
-const alvinIndex = async (req: Request, res: Response) => {
-  // Alvin's solution here
+export const alvinIndex = async (req: Request, res: Response) => {
+  try {
+    // throw new Error('internal server error'); test for status code of 500
+    // Retrieve all entries from the "UserModel" table
+    const users = await prisma.userModel.findMany();
+
+    // if (users.length >= 1) test for status code of 500
+    if (users.length === 0) {
+      // If no entries exist, respond with a status code of 400
+      return res.status(400).send("No entries found.");
+    }
+
+    // Respond with a 200 status code and the data if successful
+    res.status(200).json(users);
+  } catch (error) {
+    // If the function fails, respond with a status code of 500
+    res.status(500).send("Internal server error.");
+  }
 };
 
-const nimalanIndex = async (req: Request, res: Response) => {
-  // Nimalan's solution here
-};
+export const alvinCreate = async (req: Request, res: Response) => {
+  try {
+    // Extract the name from the request body
+    const { name } = req.body;
 
-//Problem 2: write a function that will create a new entry in the "Status" table. The request "req" will have a body of {"name": "xxx"}. Respond to the HTTP request with a 200 status code if successful. If unsuccessful, respond with a status code of 500.
-const alvinCreate = async (req: Request, res: Response) => {
-  // Alvin's solution here
-};
+    // Create a new entry in the "Status" table
+    await prisma.status.create({
+      data: {
+        name: name,
+      },
+    });
 
-const nimalanCreate = async (req: Request, res: Response) => {
-  // Nimalan's solution here
+    // Respond with a 200 status code if successful
+    res.sendStatus(200);
+  } catch (error) {
+    // If unsuccessful, respond with a status code of 500
+    res.status(500).send("Internal server error.");
+  }
 };
-
-export default { alvinIndex, nimalanIndex, alvinCreate, nimalanCreate };
