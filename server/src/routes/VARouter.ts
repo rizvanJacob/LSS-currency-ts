d5b865ba3c35035adc197859038f3953a5ab095b
Theawmaster <105071602+Theawmaster@users.noreply.github.com>
2023-06-08 08:34:06 +0000

    new-VA-Alvin

--- server/src/routes/VARouter.ts
+++ server/src/routes/VARouter.ts
@@ -1,14 +1,17 @@
 import express from "express";
+import { alvinIndex, alvinCreate } from '../controllers/VAController';
 const router = express.Router();
 
-//Problem 1: Configure the router such that it will direct a get request to the the index function within VAController.
+
+// Problem 1: Configure the router such that it will direct a GET request to the index function within VAController.
+
+// Problem 2: Configure the router such that it will direct a POST request to call the create function within VAController.
 
 //Alvin: The URL to access the route should be "/api/VA/alvin" (get)
-//Nimalan: The URL to access the route should be "/api/VA/nimalan" (get)
-
-//Problem 2: Configure the router that such that it will direct a post request to call the create function within VAcontroller.
+router.get('/alvin', alvinIndex);
 
 //Alvin: The URL to access the route should be "/api/VA/alvin" (post)
-//Nimalan: The URL to access the route should be "/api/VA/nimalan" (post)
+router.post('/alvin', alvinCreate);
+
 
 export default router;
