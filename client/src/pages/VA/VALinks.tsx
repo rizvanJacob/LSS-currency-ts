6f6d933f07ad6b8c7f043e7b5c730f064aee1b60
Theawmaster <105071602+Theawmaster@users.noreply.github.com>
2023-06-09 03:22:37 +0000

    Alvin_Prob3: Modified VARoutes.tsx and added Alvin.tsx

--- client/src/pages/VA/VALinks.tsx
+++ client/src/pages/VA/VALinks.tsx
@@ -7,9 +7,6 @@ const VALinks = () => {
         <Link to="/VA/alvin" className="btn btn-secondary btn-wide">
           Alvin
         </Link>
-        <Link to="/VA/nimalan" className="btn btn-secondary btn-wide">
-          Nimalan
-        </Link>
       </div>
     </>
   );
