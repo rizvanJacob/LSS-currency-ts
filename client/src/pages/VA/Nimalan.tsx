cc6c57f004e945382b2a5f11d3c4f6bff52dadee
Rizvan <rizzy@live.com.sg>
2023-06-14 09:24:01 +0800

    Riz comments on Nimalan code

--- client/src/pages/VA/Nimalan.tsx
+++ client/src/pages/VA/Nimalan.tsx
@@ -1,46 +1,72 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const userPage = () => {
-    const [users, setUsers] = useState([])
+  //RIZ: good use of useState including the naming conventions.
+  // However, since this is typescript, you will want to define the
+  // type of object the array should hold.
+  // You'll need to create a custom "User" type or interface.
+  const [users, setUsers] = useState([]);
 
-    const fetchUserData = () => {
-        fetch("/api/VA/nimalan")
-          .then(response => {
-            return response.json()
-          })
-          .then(data => {
-            setUsers(data)
-          })
-      }
+  const fetchUserData = () => {
+    fetch("/api/VA/nimalan")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      });
+  };
 
-      useEffect(() => {
-        fetchUserData()
-      }, []);
+  //good use of useEffect. However, since you used the .then() syntax,
+  // you don't need to define fetchUserData() as a separate function
+  useEffect(() => {
+    fetchUserData();
+    //you could have directly put in the fetch statement here, like so:
+    // fetch("/api/VA/nimalan")
+    //   .then(response => {
+    //     return response.json()
+    //   })
+    //   .then(data => {
+    //     setUsers(data)
+    //   })
 
-      return (
-        <div>
-          <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Open ID</th>
-              <th>Display Name</th>
-              <th>Account Type Name</th>
-              </tr>
-              </thead>
-              <tbody>
-        {users.map((user: any) => (
+    //another way to do it would be the async/await syntax,
+    // which is what Alvin did. the async/await syntax is
+    // now considered best practice as it is more readable.
+  }, []);
+
+  return (
+    <div>
+      <table>
+        {/* Additional Task: make the <thead></thead> element 
+        and its children a separate component */}
+        <thead>
           <tr>
-            <td>{user.id}</td>
-            <td>{user.openId}</td>
-            <td>{user.displayName}</td>
-            <td>{user.accountTypes.name}</td>
+            <th>ID</th>
+            <th>Open ID</th>
+            <th>Display Name</th>
+            <th>Account Type Name</th>
           </tr>
+        </thead>
+        <tbody>
+          {/* RIZ: Good use of map here. However, you probably 
+          don't want to use the "any" type as that negates 
+          the benefits of a typed language like typescript. 
+          To fix this you will have to define a custome User type or interface. */}
+          {users.map((user: any) => (
+            // Additional Task: make the <tr></tr> element and its children
+            // a separate component. The component will have to take in a user prop.
+            <tr>
+              <td>{user.id}</td>
+              <td>{user.openId}</td>
+              <td>{user.displayName}</td>
+              <td>{user.accountTypes.name}</td>
+            </tr>
           ))}
-          </tbody>
-        </table>        
-      </div>
-      );
-    }
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-  export default userPage;
+export default userPage;
