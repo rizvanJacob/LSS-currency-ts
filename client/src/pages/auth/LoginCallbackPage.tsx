9890de43bfbbd4826c31b51f6285ef8f2868655d
Nimalan <nimalan2012@gmail.com>
2023-06-21 19:27:07 +0800

    Logout timer function

--- client/src/pages/auth/LoginCallbackPage.tsx
+++ client/src/pages/auth/LoginCallbackPage.tsx
@@ -3,7 +3,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import { CurrentUser, setCurrentUserProp } from "../../@types/currentUser";
 import { buildFullUrl } from "../../utilities/stringManipulation";
-
+import { createLogoutTimeout } from "../../utilities/accountUtils";
 import { Account, JWT_EXPIRIES } from "../../../../server/src/constants"; // get session tokens from respective acc types
 
 const LoginCallbackPage = ({
@@ -13,33 +13,37 @@ const LoginCallbackPage = ({
   const code = location.state.code;
   const navigate = useNavigate();
 
-  const logoutUser = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("tokenExpiration");
-    navigate("/login", { replace: true });
-  };
 
-  const checkTokenExpiration = () => {
-    const expirationTime = localStorage.getItem("tokenExpiration");
-    if (expirationTime && parseInt(expirationTime) < Date.now()) {
-      logoutUser();
-    }
-  };
+  // const logoutUser = () => {
+  //   localStorage.removeItem("token");
+  //   localStorage.removeItem("tokenExpiration");
+  //   navigate("/login", { replace: true });
+  // };
+
+  // const checkTokenExpiration = () => {
+  //   const expirationTime = localStorage.getItem("tokenExpiration");
+  //   if (expirationTime && parseInt(expirationTime) < Date.now()) {
+  //     logoutUser();
+  //   }
+  // };
 
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
     attemptLogin(signal, setCurrentUser, code, navigate);
     
+    createLogoutTimeout(10000); //Need to change this to token expiry
+
     //##RIZ: why are you checking the expiration timeout every second? 
     //you can just set a timeout to log the user out when the token expires.
     //the timeout will be the duration between now and the token expiry.
-    const checkExpirationTimeout = setInterval(checkTokenExpiration, 1000); // Check for expiration every 1 second
+    //const checkExpirationTimeout = setInterval(checkTokenExpiration, 1000); 
     
     
     return () => {
       //##RIZ: remember to clear the timeout here. 
-      clearInterval(checkExpirationTimeout);
+      const clearLogoutTimeout = createLogoutTimeout(10000);
+      clearLogoutTimeout();
       controller.abort;
     };
   }, []);
@@ -62,7 +66,6 @@ const attemptLogin = async (
     const data = await response.json();
     const { token } = data;
     localStorage.setItem("token", token);
-
     const currentUser = jwt_decode(token) as CurrentUser;
 
     //##RIZ: why are you storing these as new variables? they are accessible from currentUser.
@@ -74,11 +77,11 @@ const attemptLogin = async (
       expirationTime.getSeconds() + parseInt(JWT_EXPIRIES[accountType])
     );
 
-    localStorage.setItem("token", token);
-    //##RIZ: why are you storing the expiration time in local storage?
-    //the expiration is already stored in the token (just encoded).
-    //look at lines 62-64 in App.tsx to see how to access it.
-    localStorage.setItem("tokenExpiration", expirationTime.getTime().toString());
+    // localStorage.setItem("token", token);
+    // //##RIZ: why are you storing the expiration time in local storage?
+    // //the expiration is already stored in the token (just encoded).
+    // //look at lines 62-64 in App.tsx to see how to access it.
+    // localStorage.setItem("tokenExpiration", expirationTime.getTime().toString());
 
     setCurrentUser(currentUser);
 
