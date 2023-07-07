3b7172623cfaaa20d5546a9f22e8b104a761e8f4
Aw Yong Alvin <theawmaster@Aws-MacBook-Air.local>
2023-07-07 21:22:10 +0800

    latest update on calendar view, edited files are CalendarView and AllTrainingsPage

--- client/src/pages/trainings/index/AllTrainingsPage.tsx
+++ client/src/pages/trainings/index/AllTrainingsPage.tsx
@@ -8,12 +8,16 @@ import { CurrentUser } from "../../../@types/currentUser";
 import { CurrentUserContext, TitleContext, MergedFilterContext } from "../../../App";
 import ProgressBar from "../../../components/ProgressBar";
 import TrainingsFilterControls from "./components/TrainingsFilterControls";
+import CalendarView from "../components/CalendarView"; // updated
+import dayjs from "dayjs";  // added
 
 export default function AllTrainingsPage(): JSX.Element {
   const { filterOptions, setFilterOptions } = useContext(MergedFilterContext);
   const [trainings, setTrainings] = useState<Training[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const currentUser = useContext<CurrentUser | null>(CurrentUserContext);
+  const [selectedDate, setSelectedDate] = useState(new Date());  // added
+  
   const setTitle = useContext<React.Dispatch<
     React.SetStateAction<string>
   > | null>(TitleContext);
@@ -23,6 +27,10 @@ export default function AllTrainingsPage(): JSX.Element {
     getRequest(`/api/trainings`, setTrainings).then(() => setIsLoading(false));
   }, []);
 
+  const handleFocus = (value: any) => {  // added
+    setSelectedDate(value);
+  };
+
   return isLoading ? (
     <ProgressBar />
   ) : (
@@ -30,6 +38,13 @@ export default function AllTrainingsPage(): JSX.Element {
       <div className="flex-1 overflow-auto scrollbar-hide">
         <TrainingsFilterControls
           trainings={trainings}
+        />
+        <CalendarView  // added
+          trainings={trainings}
+          displayDate={selectedDate}
+          setDisplayDate={setSelectedDate}
+          handleFocus={handleFocus}
+          currency="DFS"  // Replace "DFS" with the current selected currency
         />
         {trainings.length > 0 ? (
           <TrainingList
@@ -65,6 +80,5 @@ const filterTrainings = (
       requirementFilter = true;
     }
     return completeFilter && requirementFilter;
-
   });
 };
